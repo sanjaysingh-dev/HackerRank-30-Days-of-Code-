513e21e7a107a4075406a57d58c17602e9d725c1
sanjaysingh-dev <contact.sanjaysingh23@gmail.com>
2026-01-19 16:54:04 +0530

    Day 0 Hello World solution

---
+++ Day0_HelloWorld/Solution.java
@@ -0,0 +1,15 @@
+package Day0_HelloWorld;
+import java.util.*;
+
+public class Solution {
+    public static void main(String []args){
+    Scanner sc = new Scanner(System.in);
+    String input = sc.nextLine();
+
+   sc.close();
+   
+   System.out.println("Hello, World.");
+   System.out.println(input);
+
+}
+}
