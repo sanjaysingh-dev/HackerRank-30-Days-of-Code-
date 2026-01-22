04615e7da8b0a97fa28b2aa24075ad6f526c140e
sanjaysingh-dev <contact.sanjaysingh23@gmail.com>
2026-01-22 18:43:23 +0530

    Added Day 3 Conditional Statements Solution

---
+++ Day3_ConditionalStatements/Solution.java
@@ -0,0 +1,27 @@
+package Day3_ConditionalStatements;
+import java.util.*;
+
+public class Solution {
+ public static void main(String []args)
+ {
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    
+    sc.close();
+
+  if(N%2!=0)
+    System.out.println("Weird");
+
+  else if(N%2==0)
+  {
+    if(N>=2 && N<=5)
+        System.out.println("Not Weird");
+
+    else if(N>=6 && N<=20)
+        System.out.println("Weird");
+
+    else
+        System.out.println("Not Weird");
+  }
+}
+}
