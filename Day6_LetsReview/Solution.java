2b461ad33048126b4aa5dcb70b8793d53d245b62
sanjaysingh-dev <contact.sanjaysingh23@gmail.com>
2026-01-24 14:43:48 +0530

    Added Day 6 Let's Review Solution

---
+++ Day6_LetsReview/Solution.java
@@ -0,0 +1,32 @@
+package Day6_LetsReview;
+import java.util.*;
+
+ public class Solution{
+    
+  public static void main(String []args){
+     
+    Scanner sc = new Scanner(System.in);
+    
+    int T = sc.nextInt();
+    
+    sc.nextLine();
+    
+  for(int i = 1;i<=T;i++){
+        
+    String S = sc.nextLine();
+    String A="";
+    String B="";
+    
+    for(int j=0;j<S.length();j++)
+    {
+        if(j%2==0)
+         A=A+S.charAt(j);
+         
+        else if(j%2!=0)
+         B=B+S.charAt(j);
+    }
+   System.out.println(A+" "+B); 
+  }
+ sc.close(); 
+  } 
+ }   
