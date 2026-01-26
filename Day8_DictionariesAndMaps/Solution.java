c23486cb901d792797a562cdcba226b8f9a94305
sanjaysingh-dev <contact.sanjaysingh23@gmail.com>
2026-01-26 19:20:31 +0530

    Added Day 8 Dictionaries and Maps Solution

---
+++ Day8_DictionariesAndMaps/Solution.java
@@ -0,0 +1,33 @@
+package Day8_DictionariesAndMaps;
+import java.util.*;
+
+public class Solution{
+    public static void main(String []args){
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        Map<String,Integer> mymap = new HashMap<String,Integer>();
+
+        for(int i=0;i<N;i++){
+            String name = sc.next();
+            int number = sc.nextInt();
+            sc.nextLine();
+            mymap.put(name,number);
+        }
+
+        while(sc.hasNext()){
+            String query = sc.next();
+
+            if(mymap.containsKey(query))
+            System.out.println(query+"="+mymap.get(query));
+
+            else
+            System.out.println("Not found");    
+        }
+
+        sc.close();
+    }
+}
+
+
